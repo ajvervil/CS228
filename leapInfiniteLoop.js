8f9e125454eeaea011b6c7d8b4cf4383a018ce3c
ajvervil <Alexandre.Verville@uvm.edu>
2020-09-05 21:47:00 -0400

    Frame hands works (55)

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -4,12 +4,21 @@ var i = 0;
 
 Leap.loop(controllerOptions, function(frame)
 {
-  console.log("Hello begining of Leap.loop");
+  //console.log(frame);
+//  console.log(frame.hands[i]);
+for(var h = 0; h < frame.hands.length; h++){
+    var hand = frame.hands[h];
+    console.log(frame.hands[h]);
+}
+    i++;
+/*
+  console.log("Hello begining of Leap.loop"); //DEBUG
 
   console.log(i);
   i++;
   clear();
   draw();
-  console.log("Hello end Leap.loop");
+  console.log("Hello end Leap.loop"); //DEBUG
+  */
 }
 );
